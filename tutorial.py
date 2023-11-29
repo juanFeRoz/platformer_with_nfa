44ead58c064ce4c5e46e28cdfc2811413a876a90
Juan Felipe Rojas <juanfeliperojas2@protonmail.com>
2023-11-29 10:55:55 -0500

    Agregue el sprite, pero me sale un error al cargarlo

--- tutorial.py
+++ tutorial.py
@@ -17,16 +17,46 @@ PLAYER_VEL = 5
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
+def flip(sprites):
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
+
+def load_sprite_sheets(dir1, dir2, width, height, direction=False):
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+
+    all_sprites = {}
+
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+
+        if direction:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+        else:
+            all_sprites[image.replace(".png", "")] = sprites
+
+    return all_sprites
+
+
 class Player(pygame.sprite.Sprite):
     COLOR = (0, 0, 0)
     GRAVITY = 1
+    SPRITES = load_sprite_sheets("MainCharacters", "MaskDude", 32, 32)
 
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
         self.x_vel = 0
         self.y_vel = 0
         self.mask = None
-        self.direction = "left"
+        self.direction = "right"
         self.animation_count = 0
         self.fall_count = 0
 
@@ -47,13 +77,14 @@ class Player(pygame.sprite.Sprite):
             self.animation_count = 0
 
     def loop(self, fps):
-        self.y_vel += min (1, (self.fall_count / fps) * self.GRAVITY)
+        self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
 
         self.fall_count += 1
 
     def draw(self, win):
-        pygame.draw.rect(win, self.COLOR, self.rect)
+        self.sprite = self.SPRITES["idle_" + self.direction][0]
+        win.blit(self.sprite, (self.rect.x, self.rect.y))
 
 
 def get_background(name):
@@ -76,14 +107,16 @@ def draw(window, background, bg_image, player):
 
     pygame.display.update()
 
+
 def handle_move(player):
     keys = pygame.key.get_pressed()
 
     player.x_vel = 0
-    if keys(pygame.K_LEFT):
+    if keys[pygame.K_LEFT]:
         player.move_left(PLAYER_VEL)
-    if keys(pygame.K_RIGHT):
+    if keys[pygame.K_RIGHT]:
         player.move_right(PLAYER_VEL)
+
 
 def main(window):
     background, bg_image = get_background("Gray.png")
@@ -96,7 +129,7 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
-        
+
         player.loop(FPS)
         handle_move(player)
         draw(window, background, bg_image, player)
@@ -106,4 +139,3 @@ def main(window):
 
 if __name__ == "__main__":
     main(window)
-
